1f17b42bd87de323ef79d6fbe0b064d569d5878b
progre <djyayutto@gmail.com>
2014-04-21 20:38:46 +0900

    implement: portcheck

--- server/src/httpserver.ts
+++ server/src/httpserver.ts
@@ -1,35 +1,50 @@
-import express = require('express');
+﻿import express = require('express');
 require('express-resource');
+var session: typeof express.session = require('express-session');
+var cookieParser: typeof express.cookieParser = require('cookie-parser');
+import log4js = require('log4js');
 import functions = require('./infrastructure/functions');
-import secret = require('./secret');
+
+var logger = log4js.getLogger('server');
 
 export = HttpServer;
 class HttpServer {
     listen(port: number) {
         var app = express();
-        app.use((req: express.Request, res: express.Response, next: () => void) => {
-            functions.checkPort(req.ip, 7144)
-                .then((val) => {
-                    if (val) {
-                        next();
-                        return;
-                    }
-                    res.send(401);
-                })
-                .catch((e: any) => {
-                    console.error(e);
-                });
-        });
+        useSession(app);
+        app.use(checkPort);
         app.use(express.static(__dirname + '/public'));
-        app.resource('api/1/checkport', functions.checkPort);
-
         var server = app.listen(port, () => {
-            console.log('Listening on port %d', server.address().port);
+            logger.info('Listening on port %d', server.address().port);
         });
     }
 }
 
 function useSession(app: express.Express) {
-    app.use(express.cookieParser(secret.cookieParserSecret));
-    app.use(express.session({ secret: secret.sessionSecret }));
+    app.use(cookieParser('Heart Break'));
+    app.use(session({ secret: 'Miserable Fate' }));
+}
+
+function checkPort(req: express.Request, res: express.Response, next: () => void) {
+    var session: any = req.session;
+    if (session.portConnectable) {
+        next();
+        return;
+    }
+    logger.debug('ポート開放状況は不明です');
+    functions.checkPort(req.ip, 7144)
+        .then((val) => {
+            if (val) {
+                session.portConnectable = true;
+                logger.debug('ポートが開放されていることを確認しました');
+                next();
+                return;
+            }
+            logger.debug('ポートは解放されていません');
+            res.send(401);
+        })
+        .catch((e: any) => {
+            logger.debug('ポート開放状況の確認中にエラーが発生しました');
+            console.error(e);
+        });
 }
--- server/src/server.ts
+++ server/src/server.ts
@@ -3,9 +3,30 @@
 /// <reference path="typings/promise.d.ts"/>
 
 require('source-map-support').install();
+import fs = require('fs');
 import log4js = require('log4js');
 import HttpServer = require('./httpserver');
 
 var port = parseInt(process.argv[2], 10) || 8080;
+
+var LOG_DIRECTORY = __dirname + '/../log';
+if (!fs.existsSync(LOG_DIRECTORY)) {
+    fs.mkdirSync(LOG_DIRECTORY, '777');
+}
+log4js.configure({
+    appenders: [
+        {
+            category: 'access',
+            type: 'dateFile',
+            filename: LOG_DIRECTORY + '/access.log',
+            pattern: '-yyyy-MM-dd'
+        },
+        {
+            category: 'server',
+            type: 'dateFile',
+            filename: LOG_DIRECTORY + '/server.log',
+            pattern: '-yyyy-MM-dd'
+        }]
+});
 
 new HttpServer().listen(port);
