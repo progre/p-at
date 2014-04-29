7da24fe33a7afb68419672f5a3af77e4576b35a6
progre <djyayutto@gmail.com>
2014-04-29 15:02:53 +0900

    underconstruction...

---
+++ server/app/public/javascript/config.js
@@ -0,0 +1,8 @@
+require.config({
+  paths: {
+    'linqjs': 'vendor/linq.min',
+    'es6-shim': 'vendor/es6-shim',
+    'silverlight': 'vendor/Silverlight'
+  },
+  deps: ['main', 'es6-shim', 'silverlight']
+});
