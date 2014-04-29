efae315221f561ee70dc497a79b36d5ac615b2be
progre <djyayutto@gmail.com>
2014-04-29 15:01:55 +0900

    underconstruction...

---
+++ server/src/public/javascript/module/directives.ts
@@ -0,0 +1,73 @@
+declare var Silverlight: any;
+
+var transitionOption = ' 1.25s';
+
+// .player(.silverlight/.flash)
+//   object(silverlight/flash)
+export var silverlight = () => ({
+    restrict: 'C',
+    link: (scope: any, element: JQuery, attrs: any) => {
+        var chrome = window.navigator.userAgent.indexOf('Chrome') !== -1;
+
+        element.css({
+            top: 0,
+            width: '100%',
+            height: '100%'
+        });
+        Silverlight.createObject(
+            '/plugins/wmvplayer.xap',
+            element[0],
+            Date.now().toString(),// 一意な文字列
+            {
+                width: '100%',
+                height: '100%',
+                background: '#000',
+                version: '5.0',
+                windowless: chrome ? 'true' : 'false'// chromeはtrueじゃないとiframeとの重なりが上手く描画されない
+            },
+            {
+                onError: () => console.error('Error on Silverlight'),
+                onLoad: (sl: any, args: any) => {
+                    var ctrler = sl.Content.Controller;
+                    ctrler.addEventListener('click', () => element.click());
+                    ctrler.LocalIp = attrs.localip;
+                    ctrler.Play(attrs.streamid, attrs.remoteip);
+                }
+            },
+            null, //初期化パラメータ
+            null //onLoad イベントに渡される値
+            );
+    }
+});
+
+export function fullscreenToWindow(player: JQuery) {
+    var ctrler = (<any>$('.player object')[0]).Content.Controller;
+    // player.width():?=ctrler.width:ctrler.height
+    var newWidth = window.innerWidth * 0.618;
+    player.css({
+        width: newWidth,
+        height: newWidth * (ctrler.height || 2) / (ctrler.width || 3),
+        //transition: [
+        //    'width' + transitionOption,
+        //    'height' + transitionOption
+        //]
+    });
+    $('.dummy').css({
+        display: 'none',
+        width: window.innerWidth * 0.618,
+        height: window.innerWidth * ctrler.height / ctrler.width,
+    });
+    console.log('window');
+}
+
+export function windowToFullscreen(player: JQuery) {
+    player.css({
+        width: '100%',
+        height: '100%',
+        //transition: [
+        //    'width' + transitionOption,
+        //    'height' + transitionOption
+        //]
+    });
+    console.log('fullscreen');
+}
---
+++ server/src/public/javascript/module/playerctrler.ts
@@ -0,0 +1,29 @@
+import directives = require('./directives');
+
+export = PlayerCtrler;
+var PlayerCtrler = [
+    '$scope', '$routeParams', '$cookies',
+    ($scope: any, $routeParams: any, $cookies: any) => {
+        $('body').css({ overflow: 'visible' });// transitioníÜÇ…ïœçXÇ∑ÇÈÇ∆transitionÇ™é~Ç‹ÇÈ
+        $('.dummy').css({
+            width: '100%',
+            height: '200px'
+        });
+        var fullScreen = true;
+
+        $scope.localIp = '127.0.0.1:' + ($cookies.localPort || 7144);
+        $scope.streamId = $routeParams.streamid;
+        $scope.remoteIp = $routeParams.remoteip;
+        $('main').click(function (eventObject) {
+            if (fullScreen) {
+                directives.fullscreenToWindow($(this).children('.player'));
+                $scope.fullscreen = false;
+                fullScreen = false;
+            } else {
+                directives.windowToFullscreen($(this).children('.player'));
+                $scope.fullscreen = true;
+                fullScreen = true;
+            }
+        });
+    }
+];
