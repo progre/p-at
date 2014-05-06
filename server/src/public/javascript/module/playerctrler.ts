434ac9041087b3bf5111904567abfb4b82b725af
progre <djyayutto@gmail.com>
2014-05-06 15:09:10 +0900

    fix: iframe back

--- server/src/public/javascript/module/playerctrler.ts
+++ server/src/public/javascript/module/playerctrler.ts
@@ -4,37 +4,48 @@ export = PlayerCtrler;
 var PlayerCtrler = [
     '$scope', '$routeParams', '$cookies', '$http',
     ($scope: any, $routeParams: any, $cookies: any, $http: ng.IHttpService) => {
+        // transition中にoverflowを変更するとtransitionが止まる
+        $('.dummy').css({
+            width: '100%',
+            height: '200px'
+        });
+        $scope.fullscreen = true;
+
+        $scope.localIp = '127.0.0.1:' + ($cookies.localPort || 7144);
+        $scope.streamId = $routeParams.streamid;
+        $scope.remoteIp = $routeParams.remoteip;
+        $('main').click(function (eventObject) {
+            if ($scope.fullscreen) {
+                directives.fullscreenToWindow($(this).children('.player'));
+                $scope.fullscreen = false;
+            } else {
+                directives.windowToFullscreen($(this).children('.player'));
+                $scope.fullscreen = true;
+            }
+        });
+
         $http.get('/api/1/channels/' + $routeParams.streamid)
             .then(res => {
                 var channel = res.data.channel;
                 $scope.name = channel.name;
-                $scope.contactUrl = getMobileView(channel.url);
-
-                // transition中にoverflowを変更するとtransitionが止まる
-                $('.dummy').css({
-                    width: '100%',
-                    height: '200px'
-                });
-                $scope.fullscreen = true;
-
-                $scope.localIp = '127.0.0.1:' + ($cookies.localPort || 7144);
-                $scope.streamId = $routeParams.streamid;
-                $scope.remoteIp = $routeParams.remoteip;
-                $('main').click(function (eventObject) {
-                    if ($scope.fullscreen) {
-                        directives.fullscreenToWindow($(this).children('.player'));
-                        $scope.fullscreen = false;
-                    } else {
-                        directives.windowToFullscreen($(this).children('.player'));
-                        $scope.fullscreen = true;
-                    }
-                });
-
+                var url = getMobileView(channel.url);
+                angular.element('aside').append(getThreadViewerElement(url));
             }).catch(reason => {
                 console.error(reason);
             });
     }
 ];
+
+function getThreadViewerElement(url: string) {
+    return $('<iframe>')
+        .addClass('thread')
+        .css({
+            height: '100%',
+            width: '100%',
+            border: 'none'
+        })
+        .attr('src', url);
+}
 
 function getMobileView(url: string) {
     // したらばはliteで表示する
