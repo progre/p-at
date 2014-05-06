086c5a5a9874f80626268baa0905a1794c84cbe6
progre <djyayutto@gmail.com>
2014-05-06 17:46:02 +0900

    refactoring: plugin

--- server/src/public/javascript/module/directives.ts
+++ server/src/public/javascript/module/directives.ts
@@ -5,63 +5,93 @@ var transitionOption = ' 1.25s';
 // .player(.silverlight/.flash)
 //   object(silverlight/flash)
 export var silverlight = () => ({
-    restrict: 'C',
+    restrict: 'E',
+    replace: true,
+    template: '<div><div class="dummy" style="' + dummyStyle + '"></div></div>',
     link: (scope: any, element: JQuery, attrs: any) => {
         var chrome = window.navigator.userAgent.indexOf('Chrome') !== -1;
 
-        element.css({
-            top: 0,
-            width: '100%',
-            height: '100%'
-        });
-        Silverlight.createObject(
-            '/plugins/wmvplayer.xap',
-            element[0],
-            Date.now().toString(),// 一意な文字列
-            {
-                width: '100%',
-                height: '100%',
-                background: '#000',
-                version: '5.0',
-                windowless: chrome ? 'true' : 'false'// chromeはtrueじゃないとiframeとの重なりが上手く描画されない
-            },
-            {
-                onError: () => console.error('Error on Silverlight'),
-                onLoad: (sl: any, args: any) => {
-                    var ctrler = sl.Content.Controller;
-                    ctrler.addEventListener('click', () => element.click());
-                    ctrler.LocalIp = attrs.localip;
-                    ctrler.Play(attrs.streamid, attrs.remoteip);
+        var fullscreen = true;
+
+        var dummy = element.children('.dummy');
+        dummy.css('height', dummy.width() * 3 / 4);
+
+        var sl = getSilverlight(
+            chrome,
+            attrs.localip,
+            attrs.streamid,
+            attrs.remoteip,
+            () => element.click(),
+            (width, height) => {
+                dummy.css('height', dummy.width() * height / width);
+                if (!fullscreen) {
+                    sl.css('height', dummy.width() * height / width);
                 }
-            },
-            null, //初期化パラメータ
-            null //onLoad イベントに渡される値
-            );
+            });
+        element.append(sl);
+
+        element.click(function (eventObject) {
+            if (fullscreen) {
+                sl.css(getWindowSilverlightStyle(dummy));
+                fullscreen = false;
+            } else {
+                sl.css(fullscreenSilverlightStyle);
+                fullscreen = true;
+            }
+        });
     }
 });
 
-export function fullscreenToWindow(player: JQuery) {
-    var ctrler = (<any>$('.player object')[0]).Content.Controller;
-    // player.width():?=ctrler.width:ctrler.height
-    var newWidth = document.documentElement.clientWidth * 0.618;
-    var newHeight = newWidth * (ctrler.height || 2) / (ctrler.width || 3);
-    player.css({
-        top: $('.dummy').position().top + 1, // 1pxずれてる
-        width: newWidth,
-        height: newHeight
-    });
-    $('.dummy').css({
-        width: newWidth,
-        height: newHeight
-    });
-    console.log('window');
+function getSilverlight(chrome: boolean, localIp: string, streamId: string, remoteIp: string, onClick: Function, onMediaOpen: (width: number, height: number) => void) {
+    return $(Silverlight.createObject(
+        '/plugins/wmvplayer.xap',
+        null,
+        Date.now().toString(),// 一意な文字列
+        {
+            width: '100%',
+            height: '100%',
+            background: '#000',
+            version: '5.0',
+            windowless: chrome ? 'true' : 'false'// chromeはtrueじゃないとiframeとの重なりが上手く描画されない
+        },
+        {
+            onError: () => console.error('Error on Silverlight'),
+            onLoad: (sl: any, args: any) => {
+                var ctrler = sl.Content.Controller;
+                ctrler.addEventListener('click', onClick);
+                ctrler.addEventListener('mediaOpened', () => onMediaOpen(ctrler.width, ctrler.height));
+                ctrler.LocalIp = localIp;
+                ctrler.Play(streamId, remoteIp);
+            }
+        },
+        null, //初期化パラメータ
+        null //onLoad イベントに渡される値
+        ))
+        .css(defaultSilverlightStyle);
 }
 
-export function windowToFullscreen(player: JQuery) {
-    player.css({
-        top: 0,
-        width: '100%',
-        height: '100%'
-    });
-    console.log('fullscreen');
+var dummyStyle = 'width: 100%; background-color: #333;';
+var defaultSilverlightStyle = {
+    position: 'absolute',
+    display: 'block',
+    top: 0,
+    width: '100%',
+    height: '100%',
+    transition: 'top 0.125s, width 0.125s, height 0.125s',
+    zIndex: 2000
+};
+// transition中にoverflowを変更するとtransitionが止まる
+
+function getWindowSilverlightStyle(dummy: JQuery) {
+    return {
+        top: dummy.position().top + 1, // 1pxずれてる
+        width: dummy.width(),
+        height: dummy.height()
+    };
 }
+
+var fullscreenSilverlightStyle = {
+    top: 0,
+    width: '100%',
+    height: '100%'
+};
--- server/src/public/javascript/module/playerctrler.ts
+++ server/src/public/javascript/module/playerctrler.ts
@@ -4,25 +4,9 @@ export = PlayerCtrler;
 var PlayerCtrler = [
     '$scope', '$routeParams', '$cookies', '$http',
     ($scope: any, $routeParams: any, $cookies: any, $http: ng.IHttpService) => {
-        // transition中にoverflowを変更するとtransitionが止まる
-        $('.dummy').css({
-            width: '100%',
-            height: '200px'
-        });
-        $scope.fullscreen = true;
-
         $scope.localIp = '127.0.0.1:' + ($cookies.localPort || 7144);
         $scope.streamId = $routeParams.streamid;
         $scope.remoteIp = $routeParams.remoteip;
-        $('main').click(function (eventObject) {
-            if ($scope.fullscreen) {
-                directives.fullscreenToWindow($(this).children('.player'));
-                $scope.fullscreen = false;
-            } else {
-                directives.windowToFullscreen($(this).children('.player'));
-                $scope.fullscreen = true;
-            }
-        });
 
         // お気に入り
         $scope.favorite = false;
